f20ac71f5dc206304ea12605b716452629e409ce
123Aimu321 <aimuparker@gmail.com>
2025-07-14 20:11:21 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Section from './components/Section';
+
+function App() {
+  return (
+    <div>
+      <Section bgColor="bg-red-500">Section 1</Section>
+      <Section bgColor="bg-green-500">Section 2</Section>
+      <Section bgColor="bg-blue-500">Section 3</Section>
+      <Section bgColor="bg-yellow-500">Section 4</Section>
+    </div>
+  );
+}
+
+export default App;
